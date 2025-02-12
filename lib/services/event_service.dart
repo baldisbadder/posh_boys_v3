6d1f86cd4d46401efe2c733b80356f47d0595628
baldisbadder <vinnywakerley@gmail.com>
2025-02-12 17:15:00 +0000

    Added network client entitlement to release.ent

--- lib/services/event_service.dart
+++ lib/services/event_service.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart'; // For kDebugMode
 import '../models/event.dart';
-import 'dart:io';
 
 Future<List<Event>> fetchEvents() async {
   final url = Uri.parse('https://poshboysbar.co.uk/wp-json/mo/v1/app-events-all');
